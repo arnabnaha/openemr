38340196f61232a030f408c7485f9edcacf7bc34
Arnab Naha <superarnab@gmail.com>
2012-06-09 20:18:20 +0530

    New History Form
    Signed-off-by: Arnab Naha <superarnab@gmail.com>

---
+++ interface/forms/history_form/report.php
@@ -0,0 +1,299 @@
+<?php
+/*
+ * this file's contents are included in both the encounter page as a 'quick summary' of a form, and in the medical records' reports page.
+ */
+
+/* for $GLOBALS[], ?? */
+require_once('../../globals.php');
+/* for acl_check(), ?? */
+require_once($GLOBALS['srcdir'].'/api.inc');
+/* for generate_display_field() */
+require_once($GLOBALS['srcdir'].'/options.inc.php');
+/* The name of the function is significant and must match the folder name */
+function history_form_report( $pid, $encounter, $cols, $id) {
+    $count = 0;
+/** CHANGE THIS - name of the database table associated with this form **/
+$table_name = 'form_history';
+
+
+/* an array of all of the fields' names and their types. */
+$field_names = array('pt_name' => 'textfield','date_visit' => 'date','pt_age' => 'textfield','pt_respo' => 'dropdown_list','pt_rel' => 'dropdown_list','pt_dem' => 'dropdown_list','ch_comp' => 'textarea','pr_his' => 'textarea','past_his' => 'textarea','sleep' => 'textfield','appetite' => 'textfield','addiction' => 'dropdown_list','bowel_habit' => 'textfield','bladder_habit' => 'textfield','fam_his' => 'checkbox_list','soc_his' => 'checkbox_list','trt_his' => 'textarea','next_visit' => 'dropdown_list','app_done' => 'dropdown_list','follow_date' => 'date');/* in order to use the layout engine's draw functions, we need a fake table of layout data. */
+$manual_layouts = array( 
+ 'pt_name' => 
+   array( 'field_id' => 'pt_name',
+          'data_type' => '2',
+          'fld_length' => '30',
+          'max_length' => '255',
+          'description' => '',
+          'list_id' => '' ),
+ 'date_visit' => 
+   array( 'field_id' => 'date_visit',
+          'data_type' => '4',
+          'fld_length' => '0',
+          'description' => '',
+          'list_id' => '' ),
+ 'pt_age' => 
+   array( 'field_id' => 'pt_age',
+          'data_type' => '2',
+          'fld_length' => '10',
+          'max_length' => '255',
+          'description' => '',
+          'list_id' => '' ),
+ 'pt_respo' => 
+   array( 'field_id' => 'pt_respo',
+          'data_type' => '1',
+          'fld_length' => '0',
+          'description' => '',
+          'list_id' => 'Respondent' ),
+ 'pt_rel' => 
+   array( 'field_id' => 'pt_rel',
+          'data_type' => '1',
+          'fld_length' => '0',
+          'description' => '',
+          'list_id' => 'Relationship_list' ),
+ 'pt_dem' => 
+   array( 'field_id' => 'pt_dem',
+          'data_type' => '1',
+          'fld_length' => '0',
+          'description' => '',
+          'list_id' => 'yesno' ),
+ 'ch_comp' => 
+   array( 'field_id' => 'ch_comp',
+          'data_type' => '3',
+          'fld_length' => '50',
+          'max_length' => '4',
+          'description' => '',
+          'list_id' => '' ),
+ 'pr_his' => 
+   array( 'field_id' => 'pr_his',
+          'data_type' => '3',
+          'fld_length' => '50',
+          'max_length' => '4',
+          'description' => '',
+          'list_id' => '' ),
+ 'past_his' => 
+   array( 'field_id' => 'past_his',
+          'data_type' => '3',
+          'fld_length' => '50',
+          'max_length' => '4',
+          'description' => '',
+          'list_id' => '' ),
+ 'sleep' => 
+   array( 'field_id' => 'sleep',
+          'data_type' => '2',
+          'fld_length' => '30',
+          'max_length' => '255',
+          'description' => '',
+          'list_id' => '' ),
+ 'appetite' => 
+   array( 'field_id' => 'appetite',
+          'data_type' => '2',
+          'fld_length' => '30',
+          'max_length' => '255',
+          'description' => '',
+          'list_id' => '' ),
+ 'addiction' => 
+   array( 'field_id' => 'addiction',
+          'data_type' => '1',
+          'fld_length' => '0',
+          'description' => '',
+          'list_id' => 'addiction_status' ),
+ 'bowel_habit' => 
+   array( 'field_id' => 'bowel_habit',
+          'data_type' => '2',
+          'fld_length' => '30',
+          'max_length' => '255',
+          'description' => '',
+          'list_id' => '' ),
+ 'bladder_habit' => 
+   array( 'field_id' => 'bladder_habit',
+          'data_type' => '2',
+          'fld_length' => '30',
+          'max_length' => '255',
+          'description' => '',
+          'list_id' => '' ),
+ 'fam_his' => 
+   array( 'field_id' => 'fam_his',
+          'data_type' => '21',
+          'fld_length' => '0',
+          'description' => '',
+          'list_id' => 'hist_take' ),
+ 'soc_his' => 
+   array( 'field_id' => 'soc_his',
+          'data_type' => '21',
+          'fld_length' => '0',
+          'description' => '',
+          'list_id' => 'hist_take' ),
+ 'trt_his' => 
+   array( 'field_id' => 'trt_his',
+          'data_type' => '3',
+          'fld_length' => '60',
+          'max_length' => '4',
+          'description' => '',
+          'list_id' => '' ),
+ 'next_visit' => 
+   array( 'field_id' => 'next_visit',
+          'data_type' => '1',
+          'fld_length' => '0',
+          'description' => '',
+          'list_id' => 'yesno' ),
+ 'app_done' => 
+   array( 'field_id' => 'app_done',
+          'data_type' => '1',
+          'fld_length' => '0',
+          'description' => '',
+          'list_id' => 'yesno' ),
+ 'follow_date' => 
+   array( 'field_id' => 'follow_date',
+          'data_type' => '4',
+          'fld_length' => '0',
+          'description' => '',
+          'list_id' => '' )
+ );
+/* an array of the lists the fields may draw on. */
+$lists = array();
+    $data = formFetch($table_name, $id);
+    if ($data) {
+
+        echo '<table><tr>';
+
+        foreach($data as $key => $value) {
+
+            if ($key == 'id' || $key == 'pid' || $key == 'user' ||
+                $key == 'groupname' || $key == 'authorized' ||
+                $key == 'activity' || $key == 'date' || 
+                $value == '' || $value == '0000-00-00 00:00:00' ||
+                $value == 'n')
+            {
+                /* skip built-in fields and "blank data". */
+	        continue;
+            }
+
+            /* display 'yes' instead of 'on'. */
+            if ($value == 'on') {
+                $value = 'yes';
+            }
+
+            /* remove the time-of-day from the 'date' fields. */
+            if ($field_names[$key] == 'date')
+            if ($value != '') {
+              $dateparts = split(' ', $value);
+              $value = $dateparts[0];
+            }
+
+	    echo "<td><span class='bold'>";
+            
+
+            if ($key == 'pt_name' ) 
+            { 
+                echo xl_layout_label('Patient Name').":";
+            }
+
+            if ($key == 'date_visit' ) 
+            { 
+                echo xl_layout_label('Date of Visit').":";
+            }
+
+            if ($key == 'pt_age' ) 
+            { 
+                echo xl_layout_label('Age').":";
+            }
+
+            if ($key == 'pt_respo' ) 
+            { 
+                echo xl_layout_label('Respondent').":";
+            }
+
+            if ($key == 'pt_rel' ) 
+            { 
+                echo xl_layout_label('Relation to Patient').":";
+            }
+
+            if ($key == 'pt_dem' ) 
+            { 
+                echo xl_layout_label('Demographics Complete').":";
+            }
+
+            if ($key == 'ch_comp' ) 
+            { 
+                echo xl_layout_label('Chief Complaints').":";
+            }
+
+            if ($key == 'pr_his' ) 
+            { 
+                echo xl_layout_label('Present History').":";
+            }
+
+            if ($key == 'past_his' ) 
+            { 
+                echo xl_layout_label('Past History').":";
+            }
+
+            if ($key == 'sleep' ) 
+            { 
+                echo xl_layout_label('Sleep').":";
+            }
+
+            if ($key == 'appetite' ) 
+            { 
+                echo xl_layout_label('Appetite').":";
+            }
+
+            if ($key == 'addiction' ) 
+            { 
+                echo xl_layout_label('Addiction').":";
+            }
+
+            if ($key == 'bowel_habit' ) 
+            { 
+                echo xl_layout_label('Bowel Habit').":";
+            }
+
+            if ($key == 'bladder_habit' ) 
+            { 
+                echo xl_layout_label('Bladder Habit').":";
+            }
+
+            if ($key == 'fam_his' ) 
+            { 
+                echo xl_layout_label('Family History').":";
+            }
+
+            if ($key == 'soc_his' ) 
+            { 
+                echo xl_layout_label('Socioeconomic History').":";
+            }
+
+            if ($key == 'trt_his' ) 
+            { 
+                echo xl_layout_label('Treatment History').":";
+            }
+
+            if ($key == 'next_visit' ) 
+            { 
+                echo xl_layout_label('Follow Up Needed').":";
+            }
+
+            if ($key == 'app_done' ) 
+            { 
+                echo xl_layout_label('').":";
+            }
+
+            if ($key == 'follow_date' ) 
+            { 
+                echo xl_layout_label('Follow up date').":";
+            }
+
+                echo '</span><span class=text>'.generate_display_field( $manual_layouts[$key], $value ).'</span></td>';
+
+            $count++;
+            if ($count == $cols) {
+                $count = 0;
+                echo '</tr><tr>' . PHP_EOL;
+            }
+        }
+    }
+    echo '</tr></table><hr>';
+}
+?>
+
---
+++ interface/forms/history_form/table.sql
@@ -0,0 +1,95 @@
+CREATE TABLE IF NOT EXISTS `form_history` (
+    /* both extended and encounter forms need a last modified date */
+    date datetime default NULL comment 'last modified date',
+    /* these fields are common to all encounter forms. */
+    id bigint(20) NOT NULL auto_increment,
+    pid bigint(20) NOT NULL default 0,
+    user varchar(255) default NULL,
+    groupname varchar(255) default NULL,
+    authorized tinyint(4) default NULL,
+    activity tinyint(4) default NULL,
+    pt_name varchar(255),
+    date_visit datetime default NULL,
+    pt_age varchar(255),
+    pt_respo varchar(255),
+    pt_rel varchar(255),
+    pt_dem varchar(255),
+    ch_comp TEXT,
+    pr_his TEXT,
+    past_his TEXT,
+    sleep varchar(255),
+    appetite varchar(255),
+    addiction varchar(255),
+    bowel_habit varchar(255),
+    bladder_habit varchar(255),
+    fam_his varchar(255),
+    soc_his varchar(255),
+    trt_his TEXT,
+    next_visit varchar(255),
+    app_done varchar(255),
+    follow_date datetime default NULL,
+    PRIMARY KEY (id)
+) TYPE=InnoDB;
+INSERT IGNORE INTO list_options SET list_id='lists',
+    option_id='addiction_status',
+    title='Addiction Status';
+INSERT IGNORE INTO list_options SET list_id='addiction_status',
+    option_id='1',
+    title='Smoker for 1 to 5 years',
+    seq='1';
+INSERT IGNORE INTO list_options SET list_id='addiction_status',
+    option_id='2',
+    title='Smoker for 6 to 10 years',
+    seq='2';
+INSERT IGNORE INTO list_options SET list_id='addiction_status',
+    option_id='3',
+    title='Smoker for 11 to 15 years',
+    seq='3';
+INSERT IGNORE INTO list_options SET list_id='addiction_status',
+    option_id='4',
+    title='Smoker for 16 to 20 years',
+    seq='4';
+INSERT IGNORE INTO list_options SET list_id='addiction_status',
+    option_id='5',
+    title='Smoker for 21 to 25 years',
+    seq='5';
+INSERT IGNORE INTO list_options SET list_id='addiction_status',
+    option_id='6',
+    title='Smoker for more than 25 years',
+    seq='6';
+INSERT IGNORE INTO list_options SET list_id='addiction_status',
+    option_id='7',
+    title='Alcohol occassionally',
+    seq='7';
+INSERT IGNORE INTO list_options SET list_id='addiction_status',
+    option_id='8',
+    title='Alcohol frequently',
+    seq='8';
+INSERT IGNORE INTO list_options SET list_id='addiction_status',
+    option_id='9',
+    title='Alcohol for more than 8 years',
+    seq='9';
+INSERT IGNORE INTO list_options SET list_id='addiction_status',
+    option_id='10',
+    title='Beetel Leaf',
+    seq='10';
+INSERT IGNORE INTO list_options SET list_id='addiction_status',
+    option_id='11',
+    title='Chewing Tobacco',
+    seq='11';
+INSERT IGNORE INTO list_options SET list_id='addiction_status',
+    option_id='12',
+    title='No Addiction',
+    seq='12';
+INSERT IGNORE INTO list_options SET list_id='lists',
+    option_id='hist_take',
+    title='History Taken';
+INSERT IGNORE INTO list_options SET list_id='hist_take',
+    option_id='1',
+    title='In Demographics History Section',
+    seq='1';
+INSERT IGNORE INTO list_options SET list_id='hist_take',
+    option_id='2',
+    title='Not In Demographics History Section',
+    seq='2';
+
