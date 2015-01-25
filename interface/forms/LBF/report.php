121a2ef0e0d0f1c31a3cad23cd192c92cc9796af
Arnab Naha <superarnab@gmail.com>
2015-01-25 13:52:19 +0530

    The pdf of the LBF forms are getting cropped. Other Forms are showing nicely in the report. Fix for the issue.
    Signed-off-by: Arnab Naha <superarnab@gmail.com>

--- interface/forms/LBF/report.php
+++ interface/forms/LBF/report.php
@@ -33,7 +33,8 @@ function lbf_report($pid, $encounter, $cols, $id, $formname) {
     if ($currvalue === '') continue;
     $arr[$field_id] = $currvalue;
     // A previous change effectively did this instead, not sure if desirable? -- Rod
-    // $arr[$field_id] = wordwrap($currvalue, 30, "\n", true);
+   //  restoring the change as pdf output getting cropped --- Arnab Naha
+     $arr[$field_id] = wordwrap($currvalue, 30, "\n", true);
   }
   echo "<table>\n";
   display_layout_rows($formname, $arr);
